e59e71e8b5dfdc0f7db828268463678fbe8f9cdc
Ayush Maheshwari <111050424+FrostScythe@users.noreply.github.com>
2025-09-16 21:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
 
-  //your code here
+  if (num === 0) return ""; // Romans had no zero
 
+  let result = "";
+
+  while (num > 0) {
+    // Subtractive rules
+    if (num >= 900) { result += "CM"; num -= 900; }
+    else if (num >= 400) { result += "CD"; num -= 400; }
+    else if (num >= 90) { result += "XC"; num -= 90; }
+    else if (num >= 40) { result += "XL"; num -= 40; }
+    else if (num >= 9) { result += "IX"; num -= 9; }
+    else if (num >= 4) { result += "IV"; num -= 4; }
+    else {
+      // Greedy approach using only your obj
+      for (let i = 0; i <= 6; i++) {
+        let [symbol, value] = obj[i];
+        if (num >= value) {
+          result += symbol;
+          num -= value;
+          break;
+        }
+      }
+    }
+  }
+
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
+// ✅ Tests
+console.log(convertToRoman(0));       // ""
+console.log(convertToRoman(36));      // XXXVI
+console.log(convertToRoman(944));     // CMXLIV
+console.log(convertToRoman(1994));    // MCMXCIV
+console.log(convertToRoman(100000));  // 'M' repeated 100 times
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
